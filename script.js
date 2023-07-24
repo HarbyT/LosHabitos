20a381f35d4d59d0e4f35f46e18dd6ede12df103
HarbyT <harby.trujillo@gmail.com>
2023-07-23 21:07:21 -0500

    se agrega info en footer y se rectifica desplazador

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const upButton = document.getElementById('up-button');
 const downButton = document.getElementById('down-button');
+const endOfPage = document.getElementById('end-of-page');
+
 let currentSectionIndex = 0;
 const sections = Array.from(document.getElementsByTagName('section'));
+
+function checkButtons() {
+    const scrollPosition = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+    const docHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition > 0) {
+        upButton.hidden = false;
+    } else {
+        upButton.hidden = true;
+    }
+
+    if (scrollPosition + windowHeight >= docHeight) {
+        downButton.hidden = true;
+    } else {
+        downButton.hidden = false;
+    }
+}
+
+window.addEventListener('scroll', checkButtons);
 
 upButton.addEventListener('click', function() {
     currentSectionIndex = Math.max(0, currentSectionIndex - 1);
@@ -12,6 +34,7 @@ downButton.addEventListener('click', function() {
     currentSectionIndex = Math.min(sections.length - 1, currentSectionIndex + 1);
     sections[currentSectionIndex].scrollIntoView({behavior: 'smooth'});
 });
+
 
 document.querySelector('h1').addEventListener('click', function() {
     this.style.color = this.style.color === 'red' ? '#333' : 'red';
